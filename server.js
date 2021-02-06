c8e6b0ba45ccea8f49e4bd1391fe2842b41d69a3
Angela Masajo <aabmasajo@gmail.com>
2021-02-06 14:42:59 -0600

    prep for deploy

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ const cors = require('cors')
 const helmet = require('helmet')
 const POKEDEX = require('./pokedex.json')
 
-// console.log(process.env.API_TOKEN)
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
-  // console.log(apiToken, authToken)
 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
@@ -57,7 +56,18 @@ function handleGetPokemon(req, res) {
 
 app.get('/pokemon', handleGetPokemon)
 
-const PORT = 8000
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
